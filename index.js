3d69d2e44dc48dfefdd7bf97c00e8e2afc616a44
Jason Stallings <jason@stallin.gs>
2015-07-06 10:00:26 -0500

    Find the elements when the button is clicked.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-b=$("#branding_header, #main_navigation");
-t=$(".ember-view.ticket.section");
-d='<div id="zenzenButton" style="position: absolute; z-index:999; font-size: 20px; top: 10px; right: 130px;">F</div>';
-$("body").append(d);
-z=$("#zenzenButton");
-z.on("click", function()
 $(document).on("ready",function()
 {
-	if (b.css("display") == "block") 
+	d='<div id="zenzenButton" style="position: absolute; z-index:999; font-size: 20px; top: 10px; right: 130px; cursor: pointer;">F</div>';
+	$("body").append(d);
+	$("#zenzenButton").on("click", function()
 	{
-		b.css("display", "none"); 
-		t.css("position", "fixed");
-		z.text("X");
-		z.css("right", "10px");
-		z.css("top", "5px");
-	}
-	else 
-	{
-		b.css("display", ""); 
-		t.css("position", "");
-		z.text("F");
-		z.css("right", "130px");
-		z.css("top", "10px");
-	}
+		b=$("#branding_header, #main_navigation");
+		t=$(".ember-view.ticket.section");
+		z=$("#zenzenButton");
+
+		if (b.css("display") == "block") 
+		{
+			b.css("display", "none"); 
+			t.css("position", "fixed");
+			z.text("X");
+			z.css("right", "10px");
+			z.css("top", "5px");
+		}
+		else 
+		{
+			b.css("display", ""); 
+			t.css("position", "");
+			z.text("F");
+			z.css("right", "130px");
+			z.css("top", "10px");
+		}
 	});
 });
