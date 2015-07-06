6bcbbf5042ca6a51abdb4c54bb08dd262993587b
Jason Stallings <jason@stallin.gs>
2015-07-06 01:21:21 -0500

    Updated JavaScript to toggle.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-$("#branding_header, #main_navigation").css("display", "none");
-$(".ember-view.ticket.section").css("position", "fixed");
+b=$("#branding_header, #main_navigation");
+t=$(".ember-view.ticket.section");
+if (b.css("display") == "block") 
+{
+	b.css("display", "none"); 
+	t.css("position", "fixed");
+}
+else 
+{
+	b.css("display", ""); 
+	t.css("position", "");
+}
