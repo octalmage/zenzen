65fc80dc926cc877b2496c76430c55feed3477dc
Jason Stallings <jason@stallin.gs>
2015-07-06 02:06:13 -0500

    Added button to Zendesk GUI.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 b=$("#branding_header, #main_navigation");
 t=$(".ember-view.ticket.section");
-if (b.css("display") == "block") 
+d='<div id="zenzenButton" style="position: absolute; z-index:999; font-size: 20px; top: 10px; right: 130px;">F</div>';
+$("body").append(d);
+z=$("#zenzenButton");
+z.on("click", function()
 {
-	b.css("display", "none"); 
-	t.css("position", "fixed");
-}
-else 
-{
-	b.css("display", ""); 
-	t.css("position", "");
-}
+	if (b.css("display") == "block") 
+	{
+		b.css("display", "none"); 
+		t.css("position", "fixed");
+		z.text("X");
+		z.css("right", "10px");
+		z.css("top", "5px");
+	}
+	else 
+	{
+		b.css("display", ""); 
+		t.css("position", "");
+		z.text("F");
+		z.css("right", "130px");
+		z.css("top", "10px");
+	}
+});
